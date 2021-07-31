2940564ee0fb35d1690fb41bcf19366c6403bab5
Shikhar <shikhar@mapmyindia.com>
2021-08-01 00:50:56 +0530

    adding spring security annotations to add role based auth to endpoints

--- src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/controllers/UsersController.java
+++ src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/controllers/UsersController.java
@@ -1,6 +1,12 @@
 package com.appsdeveloperblog.ws.api.ResourceServer.controllers;
 
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +17,12 @@ public class UsersController {
     @GetMapping("/status/check")
     public String status() {
         return "Working...";
+    }
+
+    // @Secured("ROLE_developer")
+    @PreAuthorize("hasRole('developer') or #id == #jwt.subject")
+    @DeleteMapping(path = "/{id}")
+    public String deleteUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
+        return "Deleted User with id " + id + " and JWT subject : " + jwt.getSubject();
     }
 }
