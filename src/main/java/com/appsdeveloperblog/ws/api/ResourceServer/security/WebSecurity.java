da4ed1cfe61d661fde622f2b42ae179826b1bb1d
Shikhar <shikhar@mapmyindia.com>
2021-07-31 22:29:03 +0530

    adding role based authentication

--- src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/security/WebSecurity.java
+++ src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/security/WebSecurity.java
@@ -4,21 +4,25 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected final void configure(final HttpSecurity http) throws Exception {
-        // TODO Auto-generated method stub
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+
         http.authorizeRequests() //
                 .antMatchers(HttpMethod.GET, "/users/status/check") //
-                .hasAuthority("SCOPE_profile") //
-                .anyRequest() //
+                // .hasAuthority("SCOPE_profile") //
+                .hasRole("developer").anyRequest() //
                 .authenticated() //
                 .and() //
                 .oauth2ResourceServer() //
-                .jwt(); //
-
+                .jwt() //
+                .jwtAuthenticationConverter(jwtAuthenticationConverter);
     }
 }
